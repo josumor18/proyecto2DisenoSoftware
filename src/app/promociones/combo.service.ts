0c8d07c572635cf4da5ab438c3d013ebc7c77ff1
Carlos Josue Morales Rodriguez <carlosjosum@hotmail.com>
2017-11-14 16:21:39 -0600

    Clase combo

--- src/app/promociones/combo.service.ts
+++ src/app/promociones/combo.service.ts
@@ -17,6 +17,33 @@ export class ComboService {
                  .then(response => response.json() as Combo[])
                  .catch(this.handleError);
     }
+    
+    // post("/api/combos")
+    createCombo(newCombo: Combo): Promise<void | Combo> {
+      return this.http.post(this.combosUrl, newCombo)
+                 .toPromise()
+                 .then(response => response.json() as Combo)
+                 .catch(this.handleError);
+    }
+
+    // get("/api/vendedores/:id") endpoint not used by Angular app
+
+    // delete("/api/vendedor/:id")
+    deleteCombo(delComboId: String): Promise<void | String> {
+      return this.http.delete(this.combosUrl + '/' + delComboId)
+                 .toPromise()
+                 .then(response => response.json() as String)
+                 .catch(this.handleError);
+    }
+
+    // put("/api/vendedores/:id")
+    updateCombo(putCombo: Combo): Promise<void | Combo> {
+      var putUrl = this.combosUrl + '/' + putCombo._id;
+      return this.http.put(putUrl, putCombo)
+                 .toPromise()
+                 .then(response => response.json() as Combo)
+                 .catch(this.handleError);
+    }
 
     private handleError (error: any) {
       let errMsg = (error.message) ? error.message :
