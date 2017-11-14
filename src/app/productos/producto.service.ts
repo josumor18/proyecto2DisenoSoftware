fed31ab2227686e0c02fce5b15e43bb5d3fd4018
Carlos Josue Morales Rodriguez <carlosjosum@hotmail.com>
2017-11-14 02:43:13 -0600

    Clase combo

--- src/app/productos/producto.service.ts
+++ src/app/productos/producto.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Producto } from './producto';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ProductoService {
 
-  constructor() { }
+  private productosUrl = '/api/productos';
+
+  constructor (private http: Http) {}
+  
+  // get("/api/productos")
+    getProductos(): Promise<void | Producto[]> {
+      return this.http.get(this.productosUrl)
+                 .toPromise()
+                 .then(response => response.json() as Producto[])
+                 .catch(this.handleError);
+    }
 
 }
--- src/app/promociones/combo.service.ts
+++ src/app/promociones/combo.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Combo } from './combo';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ComboService {
 
-  constructor() { }
+  private combosUrl = '/api/combos';
+
+  constructor (private http: Http) {}
+  
+  // get("/api/combos")
+    getCombos(): Promise<void | Combo[]> {
+      return this.http.get(this.combosUrl)
+                 .toPromise()
+                 .then(response => response.json() as Combo[])
+                 .catch(this.handleError);
+    }
 
 }
