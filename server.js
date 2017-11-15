876383191e35eabf986468aff5bff8c5f5546d6e
Carlos Josue Morales Rodriguez <carlosjosum@hotmail.com>
2017-11-15 11:01:25 -0600

    restaurar carpeta

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
+    
   }
 
   // Save database object from the callback for reuse.
