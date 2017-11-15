2c9428cf218d54f141574221b0cbd80d2a98da18
Diego Guzmán <Diego Guzmn>
2017-11-15 12:36:08 -0600

    pull diego

--- src/app/productos/producto-list/producto-list.component.ts
+++ src/app/productos/producto-list/producto-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from '../producto';
 import { ProductoService } from '../producto.service';
-import { Combo } from 'app/promociones/combos/combo';
+import { Combo } from 'app/promociones/combo';
 
 @Component({
   selector: 'producto-list',
