49368d4059ff042cb8c6d50071af1e710c90ce80
Carlos Josue Morales Rodríguez <carlosjosum@hotmail.com>
2017-11-15 18:06:04 -0600

    Sin clase descuento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,6 @@ import { VendedorListComponent } from './vendedores/vendedor-list/vendedor-list.
 import { ComboListComponent } from './promociones/combo-list/combo-list.component';
 import { ComboDetailsComponent } from './promociones/combo-details/combo-details.component';
 import { ProductoListComponent } from './productos/producto-list/producto-list.component';
-import { DescuentoDetailsComponent } from './promociones/descuento-details/descuento-details.component';
-import { DescuentoListComponent } from './promociones/descuento-list/descuento-list.component';
-
 
 @NgModule({
   declarations: [
@@ -20,9 +17,7 @@ import { DescuentoListComponent } from './promociones/descuento-list/descuento-l
     VendedorListComponent,
     ComboListComponent,
     ComboDetailsComponent,
-    ProductoListComponent,
-    DescuentoDetailsComponent,
-    DescuentoListComponent
+    ProductoListComponent
   ],
   imports: [
     BrowserModule,
